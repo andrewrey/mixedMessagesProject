c5b53cf0f4e19e0a4aca434ab73cb56c6128a9a2
Andrew Reynolds <andrew_reynolds@mac.com>
2021-01-10 14:07:46 -0800

    added console log of quote for a response o fyes

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
-const readline = require('readline');
-const chalk = require('chalk');
+const readline = require("readline");
+const chalk = require("chalk");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 let name;
-const quotes = ['Wow, no way!!!', 'For sure man', 'You\'ve got to be kidding me???', 'Just do it!!!'];
+const quotes = [
+  "Wow, no way!!!",
+  "For sure man",
+  "You've got to be kidding me???",
+  "Just do it!!!",
+];
 
-rl.question(chalk.bold.red.inverse('What is your name fine person??\n'), (reply)=>{
+rl.question(
+  chalk.bold.red.inverse("What is your name fine person??\n"),
+  (reply) => {
     name = reply.trim();
-    rl.question(chalk.inverse.green(`${name}, are you looking for a quote today?\n`), (reply)=>{
+    rl.question(
+      chalk.inverse.green(`${name}, are you looking for a quote today?\n`),
+      (reply) => {
         console.log(reply.trim());
-        if(reply.trim() === 'yes'){
-
+        if (reply.trim() === "yes") {
+          console.log(
+            chalk.yellow.bold(quotes[Math.floor(Math.random() * quotes.length)])
+          );
         }
         rl.close();
-    });
-})
+      }
+    );
+  }
+);
 
-rl.on('close', ()=>{
-    console.log(chalk.inverse.blue('Goodbye, come again!!!'));
-})
+rl.on("close", () => {
+  console.log(chalk.inverse.blue("Goodbye, come again!!!"));
+});
