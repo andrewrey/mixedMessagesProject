4ca936aa8e0a416edad777b906a86952d6523b5b
Andrew Reynolds <andrew_reynolds@mac.com>
2021-01-10 16:28:01 -0800

    updated logic for asking if user wants additional quote

--- main.js
+++ main.js
@@ -15,12 +15,14 @@ const quotes = [
 ];
 
 let doYouWantAnotherQuote = () => {
-  rl.question("What is you name fine customer??\n", (reply) => {
-    rl.setPrompt("Would you like a quote today?\n");
+  rl.question("What is your name, oh fine customer??\n", (reply) => {
+    name = reply.trim();
+    rl.setPrompt(`Would you like a quote today? ${name}\n`);
     rl.prompt();
     rl.on("line", (reply) => {
       if (reply.trim() === "yes") {
         console.log(quotes[Math.floor(Math.random() * quotes.length)]);
+        rl.setPrompt("Would you like another quote today?\n\n");
         rl.prompt();
       } else {
         rl.close();
